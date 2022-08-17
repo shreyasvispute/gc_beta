dca611f54ec75c1200930b1e0de2fd61e0852fa4
shreyasvispute <vispute.shreyas1@gmail.com>
2022-08-16 20:53:46 -0400

    Initial commit

---
+++ frontend/volt-react-dashboard/src/pages/components/ViewTranscripts.js
@@ -0,0 +1,172 @@
+import React, { useState, useEffect } from "react";
+import {
+  Breadcrumb,
+  Button,
+  Row,
+  Col,
+  Image,
+  Accordion,
+} from "@themesberg/react-bootstrap";
+import { useParams } from "react-router-dom";
+import { useTable } from "react-table";
+import { JsonToTable } from "react-json-to-table";
+// import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faPlus } from "@fortawesome/free-solid-svg-icons";
+import axios from "../utils/http-axios";
+// import testImage from "C:\\Projects\\GC_beta\\GC_beta\\sample_output\\Deng\\p1-t3.png"
+// let testImage = require("C:\\Projects\\GC_beta\\GC_beta\\sample_output\\Deng\\p1-t3.png")
+
+export default () => {
+  let params = useParams();
+  const [studentName, setStudentName] = useState("");
+  const [tables, setTables] = useState([]);
+  const data = React.useMemo(
+    () => [
+      {
+        col1: "Hello",
+        col2: "World",
+      },
+      {
+        col1: "react-table",
+        col2: "rocks",
+      },
+      {
+        col1: "whatever",
+        col2: "you want",
+      },
+    ],
+    []
+  );
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Column 1",
+        accessor: "col1", // accessor is the "key" in the data
+      },
+      {
+        Header: "Column 2",
+        accessor: "col2",
+      },
+    ],
+    []
+  );
+  const tableInstance = useTable({ columns, data });
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
+  useEffect(() => {
+    if (tables.length === 0) {
+      fetchData();
+      document.title = `View Transcripts`;
+      console.log("viewing transcripts of student: ");
+    }
+    console.log(studentName);
+  }, [studentName]);
+
+  const fetchData = async () => {
+    const response = await axios
+      .get(`/api/students/${params["id"]}/transcript?action=view`)
+      .then((response) => {
+        // console.log(response.data['tables']);
+        setTables(response.data["tables"]);
+        setStudentName(response.data["student_name"]);
+      })
+      .catch();
+    // console.log(tables);
+  };
+
+  return (
+    <>
+      <div className="d-lg-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+        <div className="mb-4 mb-lg-0">
+          <Breadcrumb
+            className="d-none d-md-inline-block"
+            listProps={{ className: "breadcrumb-dark breadcrumb-transparent" }}
+          >
+            <Breadcrumb.Item>
+              <FontAwesomeIcon icon={faHome} />
+            </Breadcrumb.Item>
+            <Breadcrumb.Item active>Students List</Breadcrumb.Item>
+            <Breadcrumb.Item active>Prepared Transcripts</Breadcrumb.Item>
+          </Breadcrumb>
+          <h4>Prepared Transcripts</h4>
+          <p className="mb-0">
+            You are viewing the processed transcript of student [{studentName}].
+          </p>
+        </div>
+        <div className="btn-toolbar mb-2 mb-md-0">
+          <Button variant="primary" size="sm">
+            <FontAwesomeIcon icon={faPlus} className="me-2" /> Add New Student
+          </Button>
+        </div>
+      </div>
+      <Accordion defaultActiveKey="0">
+        {tables.map((table, idx) => {
+          return (
+            <Accordion.Item eventKey={idx} key={"table-" + idx}>
+              <Accordion.Header>
+                Table {idx} on Page {table.page}
+              </Accordion.Header>
+              <Accordion.Body>
+                <Row>
+                  <Col>
+                    <Image src={table.image_path} />
+                  </Col>
+                  <Col>
+                    <JsonToTable
+                      hover
+                      className="user-table align-items-center"
+                      json={JSON.parse(table.table_data)["data"]}
+                    />
+                    {/*<Table {...getTableProps()} hover className="user-table align-items-center">*/}
+                    {/*    <thead>*/}
+                    {/*    {// Loop over the header rows*/}
+                    {/*        headerGroups.map(headerGroup => (*/}
+                    {/*            // Apply the header row props*/}
+                    {/*            <tr {...headerGroup.getHeaderGroupProps()}>*/}
+                    {/*                {// Loop over the headers in each row*/}
+                    {/*                    headerGroup.headers.map(column => (*/}
+                    {/*                        // Apply the header cell props*/}
+                    {/*                        <th {...column.getHeaderProps()}>*/}
+                    {/*                            {// Render the header*/}
+                    {/*                                column.render('Header')}*/}
+                    {/*                        </th>*/}
+                    {/*                    ))}*/}
+                    {/*            </tr>*/}
+                    {/*        ))}*/}
+                    {/*    </thead>*/}
+                    {/*    /!* Apply the table body props *!/*/}
+                    {/*    <tbody {...getTableBodyProps()}>*/}
+                    {/*    {// Loop over the table rows*/}
+                    {/*        rows.map(row => {*/}
+                    {/*            // Prepare the row for display*/}
+                    {/*            prepareRow(row)*/}
+                    {/*            return (*/}
+                    {/*                // Apply the row props*/}
+                    {/*                <tr {...row.getRowProps()}>*/}
+                    {/*                    {// Loop over the rows cells*/}
+                    {/*                        row.cells.map(cell => {*/}
+                    {/*                            // Apply the cell props*/}
+                    {/*                            return (*/}
+                    {/*                                <td {...cell.getCellProps()}>*/}
+                    {/*                                    {// Render the cell contents*/}
+                    {/*                                        cell.render('Cell')}*/}
+                    {/*                                </td>*/}
+                    {/*                            )*/}
+                    {/*                        })}*/}
+                    {/*                </tr>*/}
+                    {/*            )*/}
+                    {/*        })}*/}
+                    {/*    </tbody>*/}
+                    {/*</Table>*/}
+                  </Col>
+                </Row>
+              </Accordion.Body>
+            </Accordion.Item>
+          );
+        })}
+      </Accordion>
+    </>
+  );
+};
